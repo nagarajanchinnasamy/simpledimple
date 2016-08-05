e8b0de004fda22b1b2d96e916320185348f4c563
Nagarajan Chinnasamy <nagarajanchinnasamy@gmail.com>
2016-08-05 12:11:47 +0530

    combined all the src files into one - for simple management :-)

--- src/objects/chart/begin.js
+++
@@ -1,6 +0,0 @@
-    // Copyright: 2015 AlignAlytics
-    // License: "https://github.com/PMSI-AlignAlytics/dimple/blob/master/MIT-LICENSE.txt"
-    // Source: /src/objects/chart/begin.js
-    // Help: http://github.com/PMSI-AlignAlytics/dimple/wiki/dimple.chart
-    simpledimple.chart = function (parentSelector, chartConfig, data) {
-        var bounds, noFormats, i, n, axis, series, legend, aClass, colors, color, margins;
